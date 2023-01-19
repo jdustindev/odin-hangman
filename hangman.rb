47511d8975d1375f99b9b3587fd57107cc4513f0
jdustindev <jdustindev@gmail.com>
2023-01-19 09:37:45 -0500

    Add render logic

--- hangman.rb
+++ hangman.rb
@@ -9,12 +9,23 @@ word = word_options.sample
 # TODO set up display (should be rerendered each turn) -
 # 0/6 wrong
 # letters used: X X X X ...
+@num_wrong_guesses = 0
+@max_wrong_guesses = 6
+@wrong_guesses = []
+
+def render
+    system("clear") || system("cls")
+    puts "Wrong: #{@num_wrong_guesses}/#{@max_wrong_guesses}"
+    puts "Letters used: #{@wrong_guesses.join(", ")}"
+end
 
 gameover = false
 
 # TODO load game logic
 
 until gameover
+    render
+    gets    # pause game -- this is here to stop loop until we add more logic
     # TODO quicksave logic
     # TODO turn logic
 end
