1133c6ca50e4d9252050e702fc2f0d5770f3988f
jdustindev <jdustindev@gmail.com>
2023-01-19 09:21:13 -0500

    Set up basic code structure

---
+++ hangman.rb
@@ -0,0 +1,16 @@
+# TODO load dictionary from file
+
+# TODO select random word between 5 and 12 chars
+
+# TODO set up display (should be rerendered each turn) -
+# 0/6 wrong
+# letters used: X X X X ...
+
+gameover = false
+
+# TODO load game logic
+
+until gameover
+    # TODO quicksave logic
+    # TODO turn logic
+end
