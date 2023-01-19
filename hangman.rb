b1f632d237cf0e7d10a0580b544f2185e491a8cf
jdustindev <jdustindev@gmail.com>
2023-01-19 09:55:00 -0500

    Add basic game round logic

--- hangman.rb
+++ hangman.rb
@@ -4,7 +4,7 @@ word_options = dictionary.filter do |word|
 end
 
 # Pick a random word
-word = word_options.sample
+@word = word_options.sample
 
 # TODO set up display (should be rerendered each turn) -
 # 0/6 wrong
@@ -12,20 +12,45 @@ word = word_options.sample
 @num_wrong_guesses = 0
 @max_wrong_guesses = 6
 @wrong_guesses = []
+@correct_guesses = "_" * @word.length
+
+gameover = false
 
 def render
     system("clear") || system("cls")
+    puts @correct_guesses
     puts "Wrong: #{@num_wrong_guesses}/#{@max_wrong_guesses}"
     puts "Letters used: #{@wrong_guesses.join(", ")}"
 end
 
-gameover = false
+def get_guess
+    print "Enter a letter: "
+    gets.chomp
+end
+
+def correct?(guess)
+    @word.include? guess
+end
+
+def play_round
+    puts @word  # TODO REMOVE THIS when done testing
+    guess = get_guess
+    
+    if correct? guess
+        # TODO replace "_"'s with correct letters
+        @word.each_char.with_index do |letter, index|
+            if letter == guess
+                @correct_guesses[index] = guess
+            end
+        end
+    end
+end
 
 # TODO load game logic
 
 until gameover
     render
-    gets    # pause game -- this is here to stop loop until we add more logic
+    play_round
     # TODO quicksave logic
     # TODO turn logic
 end
